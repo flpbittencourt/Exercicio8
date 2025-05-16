f8d1a0ff286527a31b6a0c3b6a189192dda7a073
laboratorio <felipebittencourt2009@gmail.com>
2025-05-15 22:16:23 -0300

    Alteração 1

--- app/src/main/java/com/example/exercicio8/FirstFragment.java
+++ app/src/main/java/com/example/exercicio8/FirstFragment.java
@@ -8,8 +8,13 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.exercicio8.databinding.FragmentFirstBinding;
+
+import java.util.List;
 
 public class FirstFragment extends Fragment {
 
@@ -22,17 +27,25 @@ public class FirstFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+        View view = binding.getRoot();
+
+        RecyclerView recyclerView = binding.recyclerView;
+
+        List<Filme> filmes = Filme.getFilmes();
+        recyclerView.setAdapter(new FilmeAdapter(filmes));
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
+        DividerItemDecoration sep = new DividerItemDecoration(getActivity().getApplicationContext(), DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(sep);
+
+        return view;
 
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(v ->
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment)
-        );
+        binding.buttonFirst.setOnClickListener(v -> NavHostFragment.findNavController(FirstFragment.this).navigate(R.id.action_FirstFragment_to_SecondFragment));
     }
 
     @Override
